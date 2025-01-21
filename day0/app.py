cb872f366d1ae1a319c0687e9f2c924c59f6603b
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2025-01-21 20:41:12 +0530

    first commit

---
+++ day0/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/as')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/as/<name>')
+def hello_name(name):
+    return f'Hello, {name}!'
+
+@app.route('/ht')
+def index():
+    return render_template('index.html')
+
+@app.route('/ht/1', methods=['GET', 'POST'])
+def index1():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'template print, {name}!'
+    else:
+        return render_template('index1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
